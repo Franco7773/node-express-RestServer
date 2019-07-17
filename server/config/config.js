ec2810c51a92ac17db42633336e4627a483f8942
Gianfranco D'Onofrio <gian7773@gmail.com>
2019-07-16 20:13:49 -0400

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://franco7773:dgdgdg@cluster0-mk70n.mongodb.net/cafe'; 
+    urlDB = process.env.MONGO_URL; 
 }
 
 process.env.URL_DB = urlDB;
